6c157ca4bc9bd452b22cb4526fffe62a6213c105
jpablomartinez <jp.martinez.colombo@gmail.com>
2025-05-02 13:58:43 -0400

    add exercise 2

--- Clase 05-06/exercises.md
+++ Clase 05-06/exercises.md
@@ -6,7 +6,8 @@
     - Crear una variable llamada **bestRecord** con el valor 100
     - Crear un procedimiento que permita cambiar el valor de bestRecord por actualRecord
 
-2. ¿Perdí una vida o perdí el juego?
-    - Crear una variable **lives** y darle el valor 3
-    - Si la variable **lives** es menor o igual a 0 entonces pierdes el juego, mostrar en pantalla game over.
-    - Si la variable **lives** es mayor a 0 entonces mostrar en pantalla que puedes seguir jugando
+2. Mi HP está llegando al límite, ¿puedo tomar una poción?
+    - crear una variable **hp** y preguntar al usuario la vida del jugador
+    - crear una variables **pociones_hp_50** y preguntar al usuario cuántas pociones de 50hp le quedan
+    - preguntar al usuario si quiere usar una poción para recuperar salud.
+    - Su programa debe verificar si puede o no usar la poción y mostrar la vida actual de su jugador
--- Clase 05-06/if-statement.py
+++ Clase 05-06/if-statement.py
@@ -1,8 +1,37 @@
 ## Ejercicio 1; Manejo de puntaje
-actualRecord = int(input("Ingrese su puntaje: "))
-bestRecord = 100
-if actualRecord > bestRecord:
-    bestRecord = actualRecord
+#actualRecord = int(input("Ingrese su puntaje: "))
+#bestRecord = 100
+#if actualRecord > bestRecord:
+#    bestRecord = actualRecord
+#else:
+#    print("No subiste tu puntuación. L")
+#print(f"Tu mejor record es {bestRecord} puntos")
+MAXHP = 250
+MAXPOTIONS = 10
+hp = int(input("¿Cuánto hp te queda?: "))
+if hp > MAXHP:
+    print("Ese valor es inválido. Te dejaremos el máximo de 250 hp")
+    hp = MAXHP
+potions = int(input("¿Cuántas pociones te quedan?: "))
+if potions > MAXPOTIONS:
+    print("Ese valor es inválido. Te dejaremos el máximo de 10 pociones")
+    potions = MAXPOTIONS
+print("////////////////////////")
+print(f"HP: {hp}")
+print(f"Pociones: {potions}")
+print("////////////////////////")
+answer = int(input("¿Quieres usar una poción para recuperar salud?(1.Si / 2.No): "))
+if answer == 1:
+    if potions > 0:
+        if hp == MAXHP:
+            print("Tu vida está al máximo. Continua tu aventura")
+        else:
+            potions-=1
+            hp+=50
+            print("Te has curado")
+            print(f"hp actual: {hp}")
+            print(f"pociones: {potions}")
+    else:
+        print("Espero hayas guardado la partida. Continua con precaución")
 else:
-    print("No subiste tu puntuación. L")
-print(f"Tu mejor record es {bestRecord} puntos")
+    print("Continua con tu aventura")
